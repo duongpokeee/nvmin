4ce3bda138f06d9416c0dff10dd872c9fffa0b46
duongpokeee <duongdoan2503@icloud.com>
2025-02-24 15:17:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update
-apt-get install curl
+apt-get install curl -y
+apt install git -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc
 nvm install 16
